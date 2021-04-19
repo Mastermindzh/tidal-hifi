829b724b96b2665953b9ed4edc82b4fac4b0fac8
Marie <8841466+Mar0xy@users.noreply.github.com>
2021-04-19 21:29:22 +0200

    I accidently uploaded the old version woops

--- src/scripts/discord.js
+++ src/scripts/discord.js
@@ -1,11 +1,15 @@
 const discordrpc = require("discord-rpc");
 const clientId = '833617820704440341';
 const mediaInfoModule = require("./mediaInfo");
+const { ipcMain } = require("electron");
+const globalEvents = require("../constants/globalEvents");
 
 const discordModule = [];
 
 let discord;
 let rpc;
+let title;
+let isPaused;
 
 const idleStatus = {
     details: `Browsing Tidal`,
@@ -18,28 +22,8 @@ discordModule.initRPC = function () {
     rpc = new discordrpc.Client({ transport: 'ipc' });
     rpc.login({ clientId }).catch(console.error);
     discordModule.rpc = rpc;
-
-    rpc.on('ready', () => {
-        rpc.setActivity(idleStatus);
-    })
-
-    discord = setInterval(() => {
-        if (mediaInfoModule.mediaInfo.status == 'paused' && rpc) {
-            rpc.setActivity(idleStatus);
-        } else if (rpc) {
-            rpc.setActivity({
-                details: `Listening to ${mediaInfoModule.mediaInfo.title}`,
-                state: mediaInfoModule.mediaInfo.artist,
-                largeImageKey: 'tidal-hifi-icon',
-                largeImageText: 'Tidal HiFi 2.0.0',
-                buttons: [
-                    { label: "Play on Tidal", url: mediaInfoModule.mediaInfo.url }
-                ],
-                instance: false,
-            });
-        }
-    }, 15e3);
 }
+
 
 discordModule.unRPC = function () {
     clearInterval(discord);
@@ -48,5 +32,30 @@ discordModule.unRPC = function () {
     rpc = false;
     discordModule.rpc = rpc;
 }
+
+ipcMain.on(globalEvents.updateStatus, (event, arg) => {
+
+    if(mediaInfoModule.mediaInfo.status == "playing" && isPaused) title = undefined;
+
+    if (mediaInfoModule.mediaInfo.status == "playing" && mediaInfoModule.mediaInfo.title != title && rpc) {
+        isPaused ? isPaused = false : isPaused = true;
+        title = mediaInfoModule.mediaInfo.title;
+        rpc.setActivity({
+            details: `Listening to ${title}`,
+            state: mediaInfoModule.mediaInfo.artist,
+            largeImageKey: 'tidal-hifi-icon',
+            largeImageText: 'Tidal HiFi 2.0.0',
+            buttons: [
+                { label: "Play on Tidal", url: mediaInfoModule.mediaInfo.url }
+            ],
+            instance: false,
+        });
+    }
+
+    if(mediaInfoModule.mediaInfo.status == "paused" && rpc && !isPaused) {
+        isPaused = true;
+        rpc.setActivity(idleStatus);
+    }
+});
 
 module.exports = discordModule;
