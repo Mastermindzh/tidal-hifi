2f447e92b192048847e83d49cee26ecd8be8d271
rex2630 <rex2630@gmail.com>
2026-07-26 00:11:03 +0200

    Fix Sonar warning

--- src/features/titlebar/titlebar.ts
+++ src/features/titlebar/titlebar.ts
@@ -11,9 +11,7 @@ import { Logger } from "../logger";
  */
 let cachedCss: string | null = null;
 const getTitlebarCss = (): string => {
-  if (cachedCss === null) {
-    cachedCss = readFileSync(path.join(__dirname, "titlebar.css"), "utf-8");
-  }
+  cachedCss ??= readFileSync(path.join(__dirname, "titlebar.css"), "utf-8");
   return cachedCss;
 };
 
